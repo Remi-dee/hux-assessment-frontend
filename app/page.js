be371131cb4eb8bb941ece88850a36b0c3462485
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-04-23 18:12:28 +0100

    Created the homepage

--- app/page.js
+++ app/page.js
@@ -10,17 +10,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-5xl font-bold">Welcome to Contact Manager</h1>
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center mb-5">
+        <h1 className="mt-[40px] lg:mt-0 lg:w-0 w-[410px] text-5xl font-bold">
+          Welcome to Contact Manager
+        </h1>
 
-        <p className="mt-3 text-xl">
+        <p className="mt-6 lg:mt-3 text-xl">
           A simple web application to manage your contacts
         </p>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
           <a
             href="/login"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-indigo-600 focus:text-indigo-600"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-indigo-600 focus:text-indigo-600 hover:border-gray-400 focus:border-gray-400 "
           >
             <h3 className="text-2xl font-bold">Login &rarr;</h3>
             <p className="mt-4 text-xl">Log in to access your contacts</p>
@@ -28,7 +30,7 @@ export default function Home() {
 
           <a
             href="/signup"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-indigo-600 focus:text-indigo-600"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-indigo-600 focus:text-indigo-600 hover:border-gray-400 focus:border-gray-400 "
           >
             <h3 className="text-2xl font-bold">Signup &rarr;</h3>
             <p className="mt-4 text-xl">Sign up to create an account</p>

1c01f87b045c9adeb06813c94d036a607ac6c20c
Your Name <you@example.com>
2024-04-28 22:57:39 +0100

    Fixed suspense error for deployment

--- app/contact/layout.js
+++ app/contact/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "../../app/globals.css";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`bg-gray-100 ${inter.className}`}>{children}</body>
+      <Suspense>
+        <body className={`bg-gray-100 ${inter.className}`}>{children}</body>
+      </Suspense>
     </html>
   );
 }
